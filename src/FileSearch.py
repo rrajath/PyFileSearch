71dd1f653e4d8be65e50437ea20c5348df8e95c3
rrajath <r.rajath@gmail.com>
2013-09-16 22:50:26 -0500

    Initial Commit.
    Details of changes:
        - Python library that can be used for searching files and folders.
        - Filetype can be included as one of the search criteria.
        - different functions exist for returning the results as a list or
          dictionary.
        - added tst folder to gitignore

---
+++ src/FileSearch.py
@@ -0,0 +1,61 @@
+'''
+Created on Sep 16, 2013
+
+@author: rajath
+'''
+import os, fnmatch
+
+# This function takes rootpath and pattern as argument and returns a 'list' of filenames that match the pattern
+# For example, if you search for all the *.py files in your workspace, then it returns a list of strings which
+# are full paths to these files
+def rec_dir_list(rootpath, pattern):
+    files = []
+    for root, dirs, files in os.walk(rootpath):
+        print type(dirs)
+        for filename in fnmatch.filter(files, pattern):
+            files.append(os.path.join(root, filename))
+    return files
+#end
+
+# This function takes rootpath as the argument and returns a 'list' of all files along with their full paths.
+def list_all_files(rootpath):
+    all_files = []
+    for root, dirs, files in os.walk(rootpath):
+        for filename in files:
+            all_files.append(os.path.join(root, filename))
+    return all_files
+#end
+
+# This function takes rootpath as the parameter and creates a dictionary of filenames categorizing them by filetypes
+def all_files_dict(rootpath):
+    dict_files = {}
+    for root, dirs, files in os.walk(rootpath):
+        for filename in files:
+            filetype = filename[filename.rfind('.'):]
+            file_path = os.path.join(root, filename)
+            try:
+                tmp = dict_files[filetype]
+                dict_files[filetype] = tmp.append(file_path)
+            except:
+                tmp = []
+                tmp.append(file_path)
+                dict_files[filetype] = tmp
+    return dict_files
+#end
+
+# This function takes rootpath as argument and returns a dictionary with directories as keys and its files as values.
+def dirs_dict(rootpath):
+    dict_dirs = {}
+    for root, dirs, files in os.walk(rootpath):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            os.chdir(os.path.join(root, file_path[:file_path.rfind('/')]))
+            dir_path = os.getcwd()
+            try:
+                dict_dirs[dir_path].append(file_path)
+            except:
+                tmp = []
+                tmp.append(file_path)
+                dict_dirs[dir_path] = tmp
+    return dict_dirs
+#end
